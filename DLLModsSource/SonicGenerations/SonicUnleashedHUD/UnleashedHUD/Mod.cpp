e9e21605ec2aefcbb1695cf7c67934f5d4ea26ff
brianuuu <38166666+brianuuu@users.noreply.github.com>
2022-05-23 12:28:28 +0100

    [Unleashed HUD] Chip Returns priority check

--- DLLModsSource/SonicGenerations/SonicUnleashedHUD/UnleashedHUD/Mod.cpp
+++ DLLModsSource/SonicGenerations/SonicUnleashedHUD/UnleashedHUD/Mod.cpp
@@ -1,5 +1,11 @@
 extern "C" __declspec(dllexport) void Init(const char* path)
 {
+	if (GetModuleHandle(TEXT("ChipReturns.dll")))
+	{
+		MessageBox(nullptr, TEXT("'Chip Returns' mod must be higher priority than 'Sonic Unleashed HUD'!"), TEXT("Sonic Unleashed HUD"), MB_ICONERROR);
+		exit(-1);
+	}
+
 	Configuration::Read();
 
 	ArchiveTreePatcher::Install();
